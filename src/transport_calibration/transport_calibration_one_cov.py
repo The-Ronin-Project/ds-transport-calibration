cc3001bff8fa650b66ab225edbae7ef72666ac10
Nathan Becker <nathan@projectronin.com>
2023-11-29 02:16:54 +0000

    Added untested first draft as a way to backup my progress.

--- src/transport_calibration/transport_calibration_one_cov.py
+++ src/transport_calibration/transport_calibration_one_cov.py
@@ -4,19 +4,22 @@ import sklearn.linear_model
 
 
 class TransportCalibrationOneCov:
-    def __init__(self, raw_pred, labels, xvals, labels_prime, xvals_prime):
+    def __init__(self, raw_pred, labels, xvals, labels_primed=None, xvals_primed=None):
         """Initialize the calibration adjustment
 
         raw_pred -- numpy array containing the model-score vectors with shape (N,C) (N-number of examples, C-number of classes)
         labels -- numpy array of length N containing an integer 0 to C-1 indicating the label for each row of 'raw_pred'
         xvals -- numpy array of length N containing a float value for X at each example
-        labels_prime -- numpy array of length M containing labels in the primed domain
-        xvals_prime -- numpy array of length M containing a float value for X at each example in the primed domain
+        labels_primed -- (optional) numpy array of length M containing labels in the primed domain
+        xvals_primed -- (optional) numpy array of length M containing a float value for X at each example in the primed domain
 
         Note: the labels array must include some examples of every class
 
+        Note: it is optional to provide the primed-domain data during construction, but if it is None,
+              then you must provide it later by calling set_primed_distribution(...) before running inference
+
         Note: for binary classification, this class accepts simplified array shapes as follows:
-            raw_pred may be shape (N,) and then the score is assumed to correspond to C=1 (the positive class)
+              raw_pred may be shape (N,) and then the score is assumed to correspond to C=1 (the positive class)
 
         """
         # Check inputs and reshape as necessary
@@ -24,8 +27,6 @@ class TransportCalibrationOneCov:
             isinstance(raw_pred, numpy.ndarray)
             and isinstance(labels, numpy.ndarray)
             and isinstance(xvals, numpy.ndarray)
-            and isinstance(labels_prime, numpy.ndarray)
-            and isinstance(xvals_prime, numpy.ndarray)
         ):
             raise ValueError("Input values must be numpy arrays.")
         if len(raw_pred.shape) == 1 or raw_pred.shape[1] == 1:
@@ -36,30 +37,70 @@ class TransportCalibrationOneCov:
 
         # Store the number of classes
         unique_labels = numpy.unique(labels)
-        unique_labels_prime = numpy.unique(labels_prime)
         max_label = max(unique_labels)
-        max_label_prime = max(unique_labels_prime)
         len_unique = len(unique_labels)
-        len_unique_prime = len(unique_labels_prime)
         if len_unique != max_label + 1:
-            raise ValueError(f"Seems like some label examples are missing: max label = {max_label}, length = {len_unique}")
-        if len_unique_prime != max_label_prime + 1:
-            raise ValueError(f"Seems like some label_prime examples are missing: max label = {max_label_prime}, length = {len_unique_prime}")
-        if len_unique != len_unique_prime:
-            raise ValueError(f"Inconsistent number of labels between domains: {len_unique} vs. {len_unique_prime} in primed domain")
+            raise ValueError(
+                f"Seems like some label examples are missing: max label = {max_label}, length = {len_unique}"
+            )
         self._n_classes = len_unique
 
         # Construct logistic regression models needed for computing density ratios
-        self._PY_RX = sklearn.linear_model.LogisticRegression().fit(numpy.concatenate([raw_pred, xvals], axis=1), labels)
+        self._PY_RX = sklearn.linear_model.LogisticRegression().fit(
+            numpy.concatenate([raw_pred, xvals], axis=1), labels
+        )
         self._PY_X = sklearn.linear_model.LogisticRegression().fit(xvals, labels)
-        self._PY_X_prime = sklearn.linear_model.LogisticRegression().fit(xvals_prime, labels_prime)
 
+        # Process and store the primed distribution if data was provided or mark it as missing for now
+        if isinstance(labels_primed, numpy.ndarray) and isinstance(
+            xvals_primed, numpy.ndarray
+        ):
+            self.set_primed_distribution(labels_primed, xvals_primed)
+        else:
+            self._PY_X_primed = None
 
     @property
     def n_classes(self):
         """Number of classes that this calibrator was initialized for (immutable)"""
         return self._n_classes
 
+    @property
+    def ready_for_inference(self):
+        """Check that the object is fully initialized and ready for inference"""
+        if self._PY_X_primed is not None:
+            return True
+        else:
+            return False
+
+    def set_primed_distribution(self, labels_primed, xvals_primed):
+        """Fit the primed data and store the distribution for use during inference
+
+        labels_primed -- numpy array of length M containing labels in the primed domain
+        xvals_primed -- numpy array of length M containing a float value for X at each example in the primed domain
+
+        """
+        # Check inputs
+        if not (
+            isinstance(labels_primed, numpy.ndarray)
+            and isinstance(xvals_primed, numpy.ndarray)
+        ):
+            raise ValueError("Input values must be numpy arrays.")
+        unique_labels_primed = numpy.unique(labels_primed)
+        max_label_primed = max(unique_labels_primed)
+        len_unique_primed = len(unique_labels_primed)
+        if len_unique_primed != max_label_primed + 1:
+            raise ValueError(
+                f"Seems like some label_primed examples are missing: max label = {max_label_primed}, length = {len_unique_primed}"
+            )
+        if len_unique_primed != self.n_classes:
+            raise ValueError(
+                f"Inconsistent number of classes in primed domain: {self.n_classes} vs. {len_unique_primed} in primed domain"
+            )
+
+        # Learn the distribution
+        self._PY_X_primed = sklearn.linear_model.LogisticRegression().fit(
+            xvals_primed, labels_primed
+        )
 
     def calibrated_probability(self, scores, xvals):
         """Compute P'(Y=c | R,X) ie. the posterior probability that Y is class c
@@ -73,16 +114,25 @@ class TransportCalibrationOneCov:
 
         Shape of the output depends on the shape of the input 'scores'
 
+            The shape of 'xvals' does not affect the output shape. But 'xvals' shape must be congruent with 'scores' shape.
+            Unless otherwise noted, 'xvals' must have shape (N,)
+
             Multi-class (n_classes >= 3):
                 if scores.shape is (N,C) then output will have shape (N,C)      (corresponds to N examples)
                 if scores.shape is (C,) then then output will have shape (C,)   (corresponds to a single example)
+                    and xvals may be a float or have shape (1,)
 
             Binary (n_classes == 2):
                 if scores.shape is (N,2) then output will have shape (N,2)      (corresponds to multi-class case)
 
                 if scores.shape is (N,) then output will have shape (N,), and each value will be the C=1 value
                 if scores is float then the output will be float, and will be the C=1 value
+                    and xvals must also be a float
         """
+        # Ensure that this object is fully initialized for inference
+        if not self.ready_for_inference:
+            raise ValueError("Not initialized for inference.")
+
         # Define default output flags which control the shape of the output according to the input (see the docstring)
         scalar_output = False
         flatten_output = False
@@ -91,15 +141,6 @@ class TransportCalibrationOneCov:
         # Check inputs and reshape as necessary
         if self.n_classes == 2:
             # Identify invalid inputs
-            if (
-                isinstance(class_probability, numpy.ndarray)
-                and len(class_probability) > 2
-            ):
-                raise ValueError(
-                    "Trained for binary classification but got unexpected shape = {shape} for class probability.".format(
-                        shape=class_probability.shape
-                    )
-                )
             if isinstance(scores, numpy.ndarray):
                 if (len(scores.shape) == 2 and scores.shape[1] > 2) or len(
                     scores.shape
@@ -111,30 +152,19 @@ class TransportCalibrationOneCov:
                     )
 
             # Identify variations of allowed inputs
-            if (
-                isinstance(class_probability, numpy.ndarray)
-                and len(class_probability.shape) == 1
-                and class_probability.shape[0] == 1
-            ):
-                # Allowed for binary classification: temporarily replace value so that it will get adjusted in the next check
-                class_probability = float(class_probability[0])
-            if isinstance(class_probability, float):
-                # Allowed for binary classification: adjust the value and shape to be consistent with a multi-class input
-                class_probability = numpy.asarray(
-                    [1 - class_probability, class_probability]
-                )
             if isinstance(scores, float):
+                # Check that other inputs are compatible
+                if not isinstance(xvals, float):
+                    raise ValueError(
+                        "Inconsistent types for 'scores' and 'xvals' : if 'scores' is float then 'xvals' must also be."
+                    )
+
                 # This is the 'scores is float' case mentioned in the docstring
                 scalar_output = True
                 shaped_scores = numpy.asarray([1 - scores, scores]).reshape(1, 2)
-        if not scalar_output and not (
-            isinstance(scores, numpy.ndarray)
-            and isinstance(class_probability, numpy.ndarray)
-        ):
+                shaped_xvals = numpy.asarray([xvals])
+        if not scalar_output and not isinstance(scores, numpy.ndarray):
             raise ValueError("Input values must be numpy arrays.")
-
-        # Repair class_probability if it contains any exactly 0 or negative values
-        class_probability = self._repair_class_probability(class_probability)
 
         # Determine the shape of the output according to the shape of the input and the type of model
         if self.n_classes == 2 and not scalar_output and len(scores.shape) == 1:
@@ -143,75 +173,75 @@ class TransportCalibrationOneCov:
                 [1 - scores.reshape(-1, 1), scores.reshape(-1, 1)], axis=1
             )
             slice_class_1_output = True
+
+            # Confirm that xvals is an appropriate shape
+            if (
+                isinstance(xvals, numpy.ndarray)
+                and len(xvals.shape) == 1
+                and xvals.shape[0] == scores.shape[0]
+            ):
+                shaped_xvals = xvals
+            else:
+                raise ValueError(
+                    "Inconsistent types/shapes between 'scores' and 'xvals'"
+                )
         elif not scalar_output:
             # Handle the shapes of either a single score input or multiple
             if len(scores.shape) == 1:
                 # This is the scores.shape is (C,) case mentioned in the docstring
                 shaped_scores = scores.reshape(-1, self.n_classes)
                 flatten_output = True
+
+                # Adjust xvals as necessary for the accepted input shapes
+                if isinstance(xvals, float):
+                    shaped_xvals = numpy.asarray([xvals])
+                elif (
+                    isinstance(xvals, numpy.ndarray)
+                    and len(xvals.shape) == 1
+                    and xvals.shape[0] == 1
+                ):
+                    shaped_xvals = xvals
+                else:
+                    raise ValueError(
+                        "Inconsistent types/shapes between 'scores' and 'xvals'"
+                    )
             else:
                 # This is the scores.shape is (N,C) case mentioned in the docstring
                 shaped_scores = scores
+                shaped_xvals = xvals
 
-        # Compute the calibrated probability using desired ratio estimator
-        if self.ratio_estimator == "histogram":
-            # Evaluate the posterior probability: this ratio estimator only supports binary classifiers
-            prior_ratio = (1 - class_probability[1]) / (
-                1 - self.training_class_probability[1]
+        # Compute the density ratios: output has N rows to cover each example in 'scores' and C columns for the classes
+        py_x = self._PY_X.predict_proba(shaped_xvals)
+        density_ratios = (
+            self._PY_RX.predict_proba(
+                numpy.concatenate([shaped_scores, shaped_xvals], axis=1)
             )
-            posterior_probability = numpy.clip(
-                class_probability[1]
-                / (
-                    class_probability[1]
-                    + (
-                        self._ratio(shaped_scores[:, 1].flatten())
-                        - self.training_class_probability[1]
+            / py_x
+        )
+
+        # Compute the conditional class-probability ratio differences (an NxCxC matrix)
+        py_x_primed = self._PY_X_primed.predict_proba(shaped_xvals)
+        w = numpy.zeros((density_ratios.shape[0], self.n_classes, self.n_classes))
+        for c in range(self.n_classes):
+            for chi in range(self.n_classes):
+                if c != chi:
+                    w[:, c, chi] = (py_x_primed[:, chi] / py_x_primed[:, c]) - (
+                        py_x[:, chi] / py_x[:, c]
                     )
-                    * prior_ratio
-                ),
-                numpy.finfo(float).resolution,
-                1 - numpy.finfo(float).resolution,
-            )
 
-            # Reshape to (N,2) so that the output shape can be modified at end of this method via a general procedure
-            posterior_probability = numpy.concatenate(
-                [
-                    1 - posterior_probability.reshape(-1, 1),
-                    posterior_probability.reshape(-1, 1),
-                ],
-                axis=1,
-            )
-        elif self.ratio_estimator == "logistic":
-            # Precompute the density ratios: output has N rows to cover each example in 'scores' and C columns for the classes
-            density_ratios = self._ratios(shaped_scores)
+        # Evaluate the sum in the denominator for each class
+        denom_sum = []
+        for c in range(self.n_classes):
+            denom_sum.append(numpy.sum(w[:, c, :] * density_ratios, axis=1))
 
-            # Precompute the class probability ratio differences (a CxC matrix)
-            w = numpy.zeros((self.n_classes, self.n_classes))
-            for c in range(self.n_classes):
-                for chi in range(self.n_classes):
-                    if c != chi:
-                        w[c, chi] = (class_probability[chi] / class_probability[c]) - (
-                            self.training_class_probability[chi]
-                            / self.training_class_probability[c]
-                        )
+        # Output has N rows to cover each example in 'scores' and C columns for each of the class
+        denom_sum = numpy.asarray(denom_sum).transpose()
 
-            # Evaluate the sum in the denominator for each class
-            denom_sum = []
-            for c in range(self.n_classes):
-                denom_sum.append(numpy.sum(w[c, :] * density_ratios, axis=1))
+        # Assemble the rest of the denominator
+        denom = 1 + py_x * denom_sum
 
-            # Output has N rows to cover each example in 'scores' and C columns for each of the class
-            denom_sum = numpy.asarray(denom_sum).transpose()
-
-            # Assemble the rest of the denominator
-            denom = 1 + self.training_class_probability.reshape(1, -1) * denom_sum
-
-            # Compute calibrated probability: output has N rows to cover each example in 'scores' and C columns for the classes
-            posterior_probability = (
-                density_ratios * self.training_class_probability.reshape(1, -1) / denom
-            )
-        else:
-            raise ValueError("Internal inconsistency")
+        # Compute calibrated probability: output has N rows to cover each example in 'scores' and C columns for the classes
+        posterior_probability = density_ratios * py_x / denom
 
         # Return result with matching shape to input
         if scalar_output:
